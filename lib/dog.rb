1ed7fcb8254d806e21850f6cc9914594211c3a89
Elijah wines <elijahwines84@yahoo.com>
2020-03-02 16:49:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,13 +11,17 @@ class Dog
   end
 
   def self.all
-    @@all.each do |dog|
-      puts dog.name
-    end
+    @@all
   end
 
   def self.clear_all
     @@all.clear
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
   end
 
 end
